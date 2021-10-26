67188ea99a2d73bc1f24ae64a8088bfd64abe235
Sang Hung <sanghung@sangs-mbp.attlocal.net>
2021-10-26 15:56:32 -0500

    fixed pom to run testng xml

--- src/test/java/TestsCICD.java
+++ src/test/java/TestsCICD.java
@@ -7,60 +7,61 @@ public class TestsCICD extends BaseTest {
     @Test
     public void RandomTest() {
        utilities.NavigateToURL("www.google.com");
-       System.out.print("Navigated to page");
+       System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest2() {
         utilities.NavigateToURL("www.ebay.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest3() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest4() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest5() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest6() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest7() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest8() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest9() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+        System.out.println("Navigated to page");
     }
 
     @Test
     public void RandomTest10() {
         utilities.NavigateToURL("www.google.com");
-        System.out.print("Navigated to page");
+//        utilities.ClickElementByID("hello fail");
+        System.out.println("Navigated to page");
     }
 }
