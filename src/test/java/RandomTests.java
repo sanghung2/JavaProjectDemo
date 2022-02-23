14b3944b178f8e8c8f5084a00c606b40d8b6ed41
Hung Sang <hungsang@ppi-cg3yemd6r.attlocal.net>
2022-02-23 16:48:03 -0600

    cleaned up code

--- src/test/java/RandomTests.java
+++ src/test/java/RandomTests.java
@@ -16,24 +16,24 @@ public class RandomTests extends BaseTest {
         System.out.println("Navigated to page");
     }
 
-//    @Test
-//    public void RandomTest3() {
-//        utilities.NavigateToURL("www.google.com");
-//        System.out.println("Navigated to page");
-//    }
-//
-//    @Test
-//    public void RandomTest4() {
-//        utilities.NavigateToURL("www.google.com");
-//        System.out.println("Navigated to page");
-//    }
-//
-//    @Test
-//    public void RandomTest5() {
-//        utilities.NavigateToURL("www.google.com");
-//        System.out.println("Navigated to page");
-//    }
-//
+    @Test
+    public void RandomTest3() {
+        utilities.NavigateToURL("www.google.com");
+        System.out.println("Navigated to page");
+    }
+
+    @Test
+    public void RandomTest4() {
+        utilities.NavigateToURL("www.google.com");
+        System.out.println("Navigated to page");
+    }
+
+    @Test
+    public void RandomTest5() {
+        utilities.NavigateToURL("www.google.com");
+        System.out.println("Navigated to page");
+    }
+
 //    @Test
 //    public void RandomTest6() {
 //        utilities.NavigateToURL("www.google.com");
@@ -57,10 +57,106 @@ public class RandomTests extends BaseTest {
 //        utilities.NavigateToURL("www.google.com");
 //        System.out.println("Navigated to page");
 //    }
+//
+//    @Test
+//    public void RandomTest10() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest11() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest12() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest13() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest14() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest15() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest16() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest17() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest18() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest19() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest20() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest21() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest22() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest23() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest24() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
+//
+//    @Test
+//    public void RandomTest25() {
+//        utilities.NavigateToURL("www.google.com");
+//        System.out.println("Navigated to page");
+//    }
 
 //    @Test
 //    //This test will fail
-//    public void RandomTest10() {
+//    public void RandomTest50() {
 //        utilities.NavigateToURL("www.google.com");
 //        utilities.ClickElementByID("hello fail");
 //    }
