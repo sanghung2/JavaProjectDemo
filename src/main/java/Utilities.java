0d27fc985b2f3c7720fcc7f2c6f8f45ce655475d
MattMcHattonBCG <extMcHatton.Matt@bcg.com>
2022-03-07 14:03:13 -0600

    adding import modules

--- src/main/java/Utilities.java
+++ src/main/java/Utilities.java
@@ -1,9 +1,9 @@
 import org.openqa.selenium.By;
 import java.io.File;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.annotations.Test;
 
 public class Utilities extends BaseTest {
@@ -13,7 +13,7 @@ public class Utilities extends BaseTest {
             getDriver().findElement(By.id(id)).click();
         } catch (Exception e){
             File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(screenshotFile, new File("*/target/screenshots/screenshot.png"));
+            FileHandler.copy(screenshotFile, new File("*/target/screenshots/screenshot.png"));
         }
     }
 
